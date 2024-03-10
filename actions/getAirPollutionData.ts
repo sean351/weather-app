b27656ce8c658f1335fc9103cdf18a0acc680755
Sean Doran <sdoran35@gmail.com>
2024-03-10 19:30:33 -0400

    fix urls

--- actions/getAirPollutionData.ts
+++ actions/getAirPollutionData.ts
@@ -6,7 +6,7 @@ export const getAirPollutionData = async ({
   lon: string
 }) => {
   const data = await fetch(
-    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/weather/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
+    `https://${process.env.NEXT_PUBLIC_FLY_URL}/api/weather/air_pollution?lat=${lat}&lon=${lon}&appid=${process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY}`
   )
   if (!data.ok) {
     throw new Error("Failed to fetch data")
